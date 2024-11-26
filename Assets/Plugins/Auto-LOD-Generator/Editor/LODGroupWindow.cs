4d6f8c2d63b9cdf56fa5a9f5d321c2e0ed518330
frod2793 <tmdwhd0344@naver.com>
2024-11-26 10:40:36 +0900

    코드 리펙터링

--- Assets/Plugins/Auto-LOD-Generator/Editor/LODGroupWindow.cs
+++ Assets/Plugins/Auto-LOD-Generator/Editor/LODGroupWindow.cs
@@ -19,7 +19,9 @@ namespace Plugins.Auto_LOD_Generator.Editor
 
         private bool _isColider;
         public string SavePath = "Assets/";
-
+        private float minuswidth = 7f;
+        
+        
         private void OnEnable()
         {
             _hSliderValue = 1f;
@@ -27,8 +29,8 @@ namespace Plugins.Auto_LOD_Generator.Editor
             _objectSelected = false;
             _objectsToSimplify = new List<GameObject>();
             GetWindow(typeof(LODGroupWindow));
-            minSize = new Vector2(600, 800f); 
-            maxSize = new Vector2(1200, 1600f); 
+            minSize = new Vector2(600, 800f);
+            maxSize = new Vector2(1200, 1600f);
 
             _reorderableList = new ReorderableList(_objectsToSimplify, typeof(GameObject), true, true, true, true)
             {
@@ -43,116 +45,160 @@ namespace Plugins.Auto_LOD_Generator.Editor
             };
         }
 
-    private void OnGUI()
-    {
-    GUILayout.BeginArea(new Rect(0f, 0f, position.width, position.height));
-    GUILayout.BeginHorizontal(); 
-
-    GUILayout.BeginVertical(); 
-    GUILayout.Box(_icon, GUILayout.Height(140f), GUILayout.Width(140f));
-    if (GUILayout.Button("Select Save Path", GUILayout.Height(20f), GUILayout.Width(position.width-7f)))
-    {
-        SavePath = EditorUtility.OpenFolderPanel("Select Save Path", Application.dataPath, "folder");
-    }
-
-    SavePath = EditorGUILayout.TextField("Save Path:", SavePath, GUILayout.Height(20f), GUILayout.Width(position.width-7f));
-    GUILayout.Space(20);
-    _isColider = EditorGUILayout.Toggle("Colider", _isColider);
-    if (_objectSelected)
-    {
-        GUILayout.Space(20);
-        EditorGUILayout.LabelField("Quality Factor: ", GUILayout.Height(20f), GUILayout.Width(position.width-7f));
-        var textFieldVal = float.Parse(EditorGUILayout.TextField(
-            _hSliderValue.ToString(CultureInfo.InvariantCulture), GUILayout.Height(20f), GUILayout.Width(position.width)));
-
-        if (textFieldVal >= 0 && textFieldVal <= 1)
+        private void OnGUI()
         {
-            _hSliderValue = textFieldVal;
-        }
-        else
-        {
-            Debug.LogError("Quality factor number must be between 0 and 1");
-        }
+            GUILayout.BeginArea(new Rect(0f, 0f, position.width, position.height));
+            GUILayout.BeginHorizontal();
 
-        _hSliderValue = GUILayout.HorizontalScrollbar(_hSliderValue, 0.01f, 0f, 1f, GUILayout.Height(20f), GUILayout.Width(position.width-7f));
-        GUILayout.Space(20);
+            SelectPath_Btn_GUI();
 
-        if (GUILayout.Button("Generate", GUILayout.Height(20f), GUILayout.Width(position.width)))
-        {
-            foreach (var obj in _objectsToSimplify)
+
+            Toggle_Colider_GUI();
+
+            if (_objectSelected)
             {
-                LODGenerator.Generator(obj, _hSliderValue, SavePath, _isColider);
+                ObjectSelected_GUI();
             }
+
+            Select_Object_from_File_Btn_GUI();
+
+            Select_Object_from_Scene_GUI();
+
+            List_Clear_Btn_GUI();
+
+            Drag_And_Drop_GUI();
+
+            GUILayout.EndVertical();
+            GUILayout.EndVertical();
+            GUILayout.EndHorizontal();
+            GUILayout.EndArea();
         }
-    }
 
-    GUILayout.Space(20);
-    if (GUILayout.Button("Select Object from File", GUILayout.Height(20f), GUILayout.Width(position.width-7f)))
-    {
-        _objPath = EditorUtility.OpenFilePanel("Select an FBX object", Application.dataPath, "fbx").Replace(Application.dataPath, "");
-
-        if (_objPath.Length != 0)
+        private void Drag_And_Drop_GUI()
         {
-            _objectSelected = true;
-            var obj = AssetDatabase.LoadAssetAtPath("Assets/" + _objPath, typeof(GameObject)) as GameObject;
-            if (obj != null)
+            GUILayout.Space(20);
+            GUILayout.BeginVertical(GUILayout.Width(position.width), GUILayout.Height(position.height - 200));
+            _reorderableList.DoLayoutList();
+
+            var evt = Event.current;
+            if (evt.type == EventType.DragUpdated || evt.type == EventType.DragPerform)
             {
-                _objectsToSimplify.Add(obj);
-            }
-        }
-    }
-
-    GUILayout.Space(20);
-    if (GUILayout.Button("Select Object from Scene", GUILayout.Height(20f), GUILayout.Width(position.width-7f)))
-    {
-        var selectedObjects = Selection.gameObjects;
-        if (selectedObjects.Length > 0)
-        {
-            _objectSelected = true;
-            _objectsToSimplify.AddRange(selectedObjects);
-            _objPath = selectedObjects[0].name;
-        }
-        else
-        {
-            Debug.LogError("No object selected in the scene.");
-        }
-    }
-
-    GUILayout.Space(20);
-    if (GUILayout.Button("List Clear", GUILayout.Height(20f), GUILayout.Width(position.width-7f)))
-    {
-        _objectsToSimplify.Clear();
-        _objectSelected = false;
-    }
-
-    GUILayout.Space(20);
-    GUILayout.BeginVertical(GUILayout.Width(position.width), GUILayout.Height(position.height - 200));
-    _reorderableList.DoLayoutList();
-
-    var evt = Event.current;
-    if (evt.type == EventType.DragUpdated || evt.type == EventType.DragPerform)
-    {
-        DragAndDrop.visualMode = DragAndDropVisualMode.Copy;
-        if (evt.type == EventType.DragPerform)
-        {
-            DragAndDrop.AcceptDrag();
-            foreach (var draggedObject in DragAndDrop.objectReferences)
-            {
-                if (draggedObject is GameObject gameObject)
+                DragAndDrop.visualMode = DragAndDropVisualMode.Copy;
+                if (evt.type == EventType.DragPerform)
                 {
-                    _objectsToSimplify.Add(gameObject);
+                    DragAndDrop.AcceptDrag();
+                    foreach (var draggedObject in DragAndDrop.objectReferences)
+                    {
+                        if (draggedObject is GameObject gameObject)
+                        {
+                            _objectsToSimplify.Add(gameObject);
+                            _objectSelected = true;
+                        }
+                    }
+                }
+
+                Event.current.Use();
+            }
+        }
+
+        private void List_Clear_Btn_GUI()
+        {
+            GUILayout.Space(20);
+            if (GUILayout.Button("List Clear", GUILayout.Height(20f), GUILayout.Width(position.width - minuswidth)))
+            {
+                _objectsToSimplify.Clear();
+                _objectSelected = false;
+            }
+        }
+
+        private void Select_Object_from_Scene_GUI()
+        {
+            GUILayout.Space(20);
+            if (GUILayout.Button("Select Object from Scene", GUILayout.Height(20f),
+                    GUILayout.Width(position.width - minuswidth)))
+            {
+                var selectedObjects = Selection.gameObjects;
+                if (selectedObjects.Length > 0)
+                {
                     _objectSelected = true;
+                    _objectsToSimplify.AddRange(selectedObjects);
+                    _objPath = selectedObjects[0].name;
+                }
+                else
+                {
+                    Debug.LogError("No object selected in the scene.");
                 }
             }
         }
 
-        Event.current.Use();
-    }
+        private void Select_Object_from_File_Btn_GUI()
+        {
+            GUILayout.Space(20);
+            if (GUILayout.Button("Select Object from File", GUILayout.Height(20f),
+                    GUILayout.Width(position.width - minuswidth)))
+            {
+                _objPath = EditorUtility.OpenFilePanel("Select an FBX object", Application.dataPath, "fbx")
+                    .Replace(Application.dataPath, "");
 
-    GUILayout.EndVertical();
-    GUILayout.EndVertical();
-    GUILayout.EndHorizontal();
-    GUILayout.EndArea();
-}
+                if (_objPath.Length != 0)
+                {
+                    _objectSelected = true;
+                    var obj = AssetDatabase.LoadAssetAtPath("Assets/" + _objPath, typeof(GameObject)) as GameObject;
+                    if (obj != null)
+                    {
+                        _objectsToSimplify.Add(obj);
+                    }
+                }
+            }
+        }
+
+        private void Toggle_Colider_GUI()
+        {
+            GUILayout.Space(20);
+            _isColider = EditorGUILayout.Toggle("Colider", _isColider);
+        }
+
+        private void SelectPath_Btn_GUI()
+        {
+            GUILayout.BeginVertical();
+            GUILayout.Box(_icon, GUILayout.Height(140f), GUILayout.Width(140f));
+            if (GUILayout.Button("Select Save Path", GUILayout.Height(20f), GUILayout.Width(position.width - minuswidth)))
+            {
+                SavePath = EditorUtility.OpenFolderPanel("Select Save Path", Application.dataPath, SavePath);
+            }
+
+            SavePath = EditorGUILayout.TextField("Save Path:", SavePath, GUILayout.Height(20f),
+                GUILayout.Width(position.width - minuswidth));
+        }
+
+        private void ObjectSelected_GUI()
+        {
+            GUILayout.Space(20);
+            EditorGUILayout.LabelField("Quality Factor: ", GUILayout.Height(20f), GUILayout.Width(position.width - minuswidth));
+            var textFieldVal = float.Parse(EditorGUILayout.TextField(
+                _hSliderValue.ToString(CultureInfo.InvariantCulture), GUILayout.Height(20f),
+                GUILayout.Width(position.width)));
+
+            if (textFieldVal >= 0 && textFieldVal <= 1)
+            {
+                _hSliderValue = textFieldVal;
+            }
+            else
+            {
+                Debug.LogError("Quality factor number must be between 0 and 1");
+            }
+
+            _hSliderValue = GUILayout.HorizontalScrollbar(_hSliderValue, 0.01f, 0f, 1f, GUILayout.Height(20f),
+                GUILayout.Width(position.width - minuswidth));
+            GUILayout.Space(20);
+
+            if (GUILayout.Button("Generate", GUILayout.Height(20f), GUILayout.Width(position.width-minuswidth)))
+            {
+                foreach (var obj in _objectsToSimplify)
+                {
+                    LODGenerator.Generator(obj, _hSliderValue, SavePath, _isColider);
+                }
+            }
+        }
     }
 }
